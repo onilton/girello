269b35c24524ee9e8dc3dbe1d97ed70eaddc19e3
Onilton Maciel <oniltonmaciel@gmail.com>
2014-06-16 16:18:39 -0300

    Initial trello support

--- girello.py
+++ girello.py
@@ -3,6 +3,7 @@
 
 from github3 import login
 from configobj import ConfigObj
+from trello import TrelloClient
 from collections import namedtuple
 import pprint
 
@@ -10,17 +11,124 @@ pp = pprint.PrettyPrinter(indent=4)
 users_cache = {}
 
 GithubConfig = namedtuple('GithubConfig', ['token', 'identifier'])
+TrelloConfig = namedtuple(
+    'TrelloConfig',
+    ['api_key', 'api_secret', 'token', 'token_secret'],
+)
+
+
+class GirelloTrello:
+    def __init__(self, boards, config_boards):
+        # TO-DO: implement a warning when a board in config_boards does not
+        # exist in boards
+
+        self.boards = []
+
+        print "GirelloTrello config_boards"
+        pp.pprint(config_boards)
+        for board in boards:
+            print "GirelloTrello dname="+board.name
+            if board.name in config_boards:
+                board_config = config_boards[board.name]
+
+                gi_board = GirelloBoard(
+                    board,
+                    doing_list_name=board_config.get('doing_list'),
+                    review_list_name=board_config.get('review_list'),
+                    repositories=board_config.get('repositories', list()),
+                    exclude_repositories=board_config.get(
+                        'exclude_repositories',
+                        list()
+                    ),
+                )
+
+                self.boards.append(gi_board)
+
+        self.repos = {}
+        for board in self.boards:
+            for repo in board.repositories:
+                self.repos[repo] = board
+
+    def get_boards_for_repo(self, repo):
+        boards_for_repo = set()
+
+        if repo in self.repos:
+            boards_for_repo.add(self.repos[repo])
+
+        for board in self.boards:
+            if ((board.repositories is None or
+                    not board.repositories) and
+                    repo not in board.exclude_repositories):
+                # If board has no repositories set and
+                # if repo not in exclude list we should return it
+                # for this repo
+                boards_for_repo.add(board)
+
+        return boards_for_repo
+
+
+class GirelloBoard:
+    def __init__(self,
+                 board,
+                 doing_list_name=None,
+                 review_list_name=None,
+                 repositories=None,
+                 exclude_repositories=None):
+
+        self.info = board
+        self.open_lists = board.open_lists()
+        self.doing_list = None
+        self.review_list = None
+        self.done_list = None
+        self.lists_before_doing = []
+
+        self.repositories = repositories
+        self.exclude_repositories = exclude_repositories
+
+        for l in self.open_lists:
+            if self.review_list is not None and self.done_list is None:
+                self.done_list = l
+
+            if l.name == doing_list_name:
+                self.doing_list_name = l
+
+            if self.doing_list is None:
+                self.lists_before_doing.append(l)
+
+            if l.name == review_list_name:
+                self.review_list_name = l
+
+    def find_card(self, card_name):
+        for l in self.open_lists:
+            for c in l.list_cards():
+                #if c.name.find(card_name) != -1: #substring
+                if c.name == card_name:
+                    return c
+        return None
+
+    def find_list(self, list_name):
+        for l in self.open_lists:
+            for c in l.list_cards():
+                if l.name == list_name:
+                    return c
+        return None
 
 
 class ConfigSession:
     def __init__(self, config, session_name):
         self.session = config[session_name]
 
-    def get(self, option):
-        return self.session.get(option)
+    def get(self, option, default=None):
+        if default is not None:
+            return self.session.get(option, default)
+        else:
+            return self.session.get(option)
 
-    def getint(self, option):
-        return int(self.session.get(option))
+    def getint(self, option, default=None):
+        if default is not None:
+            return int(self.session.get(option, str(default)))
+        else:
+            return int(self.session.get(option))
 
 
 class GirelloConfig:
@@ -33,11 +141,20 @@ class GirelloConfig:
             identifier=github_section.get('identifier'),
         )
 
-        girello_section = ConfigSession(self.config, 'Girello')
-        self.allowed_orgs = set(
-            girello_section.get('allowed_orgs').split(',')
+        trello_section = ConfigSession(self.config, 'Trello')
+        self.trello = TrelloConfig(
+            api_key=trello_section.get('api_key'),
+            api_secret=trello_section.get('api_secret'),
+            token=trello_section.get('oauth_token'),
+            token_secret=trello_section.get('oauth_token_secret'),
         )
 
+        girello_section = ConfigSession(self.config, 'Girello')
+        self.allowed_orgs = set(
+            girello_section.get('allowed_orgs')
+        )
+
+        self.boards = dict((b['name'], b) for b in raw_boards)
 
 class CommitEvent:
     def __init__(self, **entries):
@@ -70,6 +187,12 @@ class CreateBranchEvent:
 
 config = GirelloConfig()
 gh = login(token=config.github.token)
+trello = TrelloClient(
+    api_key=config.trello.api_key,
+    api_secret=config.trello.api_secret,
+    token=config.trello.token,
+    token_secret=config.trello.token_secret,
+)
 #auth = gh.authorization(config.github.identifier)
 #print pp.pprint(gh.rate_limit())
 user = gh.user()
@@ -116,3 +239,8 @@ for org in orgs:
         #print event.to_json()
         print ""
 
+gi_trello = GirelloTrello(trello.list_boards(), config.boards)
+print "girello_trello"
+for b in girello_trello.boards:
+    print "b.id=" + b.info.id
+    print "b.name=" + b.info.name
